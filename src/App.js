0a5861f9eb42a387fe5115a75f9fb8c885f0d456
Suriya2023 <rajputsuraj221221@gmail.com>
2024-06-08 09:36:32 +0530

    remove Unwanted Files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+
 import Navbar from './componet/Navbar';
 // import FormFile from './componet/FormFile';
 import AboutUs from './componet/Aboutus';
 function App() {
   return (
-    <>  
-      <Navbar Title="Git"/>
-        {/* <FormFile FTitle="Input your text" /> */}
+    <>
+      <Navbar Title="Git" />
+      {/* <FormFile FTitle="Input your text" /> */}
 
-        <AboutUs  headinh="About-Us" />
+      <AboutUs headinh="About-Us" />
+
+
+
     </>
   );
 }
