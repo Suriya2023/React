6f9ee96a2a0c91a5257243f5afbc27eb8a999061
Suriya2023 <rajputsuraj221221@gmail.com>
2024-06-12 20:40:11 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import './App.css';
 import Navbar from './componet/Navbar';
 import FormFile from './componet/FormFile';
 import { useState } from 'react';
+import Alert from './componet/Alert'
 function App() {
 
   const [mode, setMode] = useState('light')
   const [setBtn,setBtnMode] = useState("Enable To Dark Mode")
+  const [alert , setAlert] = useState(null);
+
+
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg:message,
+      type:type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleMode = () => {
     if (mode === 'light') {
@@ -14,6 +27,8 @@ function App() {
       document.body.style.background = 'black'
       document.body.style.color = 'white'
       document.body.style.transition = 'all .5s'
+      showAlert("Dark Mode Has Been Enable" , "success");
+
     }
     else {
       setMode('light');
@@ -23,14 +38,16 @@ function App() {
       document.body.style.color = 'black'
 
       document.body.style.transition = 'all .5s'
+      showAlert("Light Mode Has Been Enable" , "success");
 
     }
   }
 
   return (
     <>
+    <Alert alert = {alert}/>
       <Navbar Title="Git" mode={mode} toggleMode={toggleMode} setBtn = {setBtn} />
-      <FormFile FTitle="Input your text" mode={mode} toggleMode ={toggleMode} />
+      <FormFile FTitle="Input your text" mode={mode} showAlert={showAlert} toggleMode ={toggleMode} />
     </>
   );
 }
---
+++ src/componet/Alert.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Alert(props) {
+    const capitalize = (word) => {
+        const lower = word.toLowerCase();
+        return lower.CharAt(0).toUpperCase() + lower.slice(1)
+    }
+  return (
+        props.alert && <div className={` alert alert-${props.alert.type} alert-dimissible fade show `}role="alert"><strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
+        </div>
+        
+        
+  )
+}
