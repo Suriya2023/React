d8d415048704dcdb704539e2efeec2ac3689de75
Suriya2023 <rajputsuraj221221@gmail.com>
2024-06-13 16:35:39 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,122 @@
-import './App.css';
-import Navbar from './components/Navbar';
-
-import FormData from './components/MyForm';
 import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm.js';
+
 function App() {
-     
-  const [mode, setMode] = useState("light")
-  const [thisBtn, SetBtn] = useState("Enable To Dark Mode")
 
-  const ToggleTheam = () => {
+  const [mode,setMode] = useState("light");
+  
+  const [btnText,setBtnText] = useState("Enable Dark Mode");
+
+  const toggleMode = () => {
     if (mode === 'light') {
-      setMode("dark")
-      SetBtn("Enable TO Dark Mode")
-      document.body.style.background = "black";
-      document.body.style.color = "white";
-
+        setMode('dark');
+        document.body.style.background= "black"
+        document.body.style.color= "white"
+        setBtnText("Enable light Mode");
     } else {
-      setMode("light")
-      SetBtn("Enable To Light Mode")
-      document.body.style.background = "white"
-      document.body.style.color = "black"
+      setMode('light');
+      document.body.style.background= "white"
+      document.body.style.color= "black"
+      setBtnText("Enable Dark Mode");
+    }
+  }
+  const redToggle = () => {
+    if (mode === 'light') {
+        setMode('red');
+        document.body.style.backgroundImage="linear-gradient(to top, #ff0844 0%, #ffb199 100%)";
+        document.body.style.color= "DarkRed"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('red');
+      document.body.style.color="linear-gradient(to top, #ff0844 0%, #ffb199 100%)";
+        document.body.style.backgroundColor= "DarkRed"
+      setBtnText("Enable Dark Mode");
     }
   }
 
+  const greenToggle = () => {
+    if (mode === 'light') {
+        setMode('green');
+        document.body.style.backgroundImage= "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)";
+        document.body.style.color= "green"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('green');
+      document.body.style.background= "green"
+      document.body.style.color= "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)"
+      setBtnText("Enable Dark Mode");
+    }
+  }
 
+  const purpleToggle = () => {
+    if (mode === 'light') {
+        setMode('blue');
+        document.body.style.backgroundImage = "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)";
+        document.body.style.color= "purple"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('blue');
+      document.body.style.background= "lightblue"
+      document.body.style.color= "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)"
+      setBtnText("Enable Dark Mode");
+    }
+  }
+
+  const yellowToggle = () => {
+    if (mode === 'light') {
+        setMode('orange');
+        document.body.style.backgroundImage = "linear-gradient(to right, #f83600 0%, #f9d423 100%)";
+        document.body.style.color= "yellow"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('orange');
+      document.body.style.background= "orange"
+      document.body.style.color= "linear-gradient(to right, #f83600 0%, #f9d423 100%)"
+      setBtnText("Enable Dark Mode");
+    }
+  }
+
+  const blueToggle = () => {
+    if (mode === 'light') {
+        setMode('blue');
+        document.body.style.backgroundImage = "linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%)";
+        document.body.style.color= "darkblue"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('blue');
+      document.body.style.background= "lightblue"
+      document.body.style.color= "linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%)"
+      setBtnText("Enable Dark Mode");
+    }
+  }
+
+  const grayToggle = () => {
+    if (mode === 'light') {
+        setMode('gray');
+        document.body.style.backgroundImage = "linear-gradient(-20deg, #616161 0%, #9bc5c3 100%)";
+        document.body.style.color= "black"
+        setBtnText("Enable light Mode");
+    } else {
+      setMode('gray');
+      document.body.style.background= "gray"
+      document.body.style.color= "white"
+      setBtnText("Enable Dark Mode");
+    }
+  }
+
+  
+
+ 
 
 
   return (
-    <>
-
-
-      <Navbar Mytitle="Suraj" mode={mode} ToggleTheam3={ToggleTheam3}   ToggleTheam6={ToggleTheam6} ToggleTheam4={ToggleTheam4} ToggleTheam5={ToggleTheam5} ToggleTheam4={ToggleTheam4} ToggleTheam={ToggleTheam} ToggleTheam2={ToggleTheam2} ToggleTheam1={ToggleTheam1} thisBtn = {thisBtn} />
-      <FormData Title="Enter your  Any Text"ToggleTheam3={ToggleTheam3}ToggleTheam6={ToggleTheam6}  ToggleTheam5={ToggleTheam5} ToggleTheam4={ToggleTheam4} mode={mode} ToggleTheam2={ToggleTheam2} ToggleTheam1={ToggleTheam1} ToggleTheam={ToggleTheam} input="input text here" />
-
+    <> 
+    <Navbar  heading="SIT" AboutText="About Us"  mode = {mode} btnText={btnText} toggleMode = {toggleMode} redToggle = {redToggle} purpleToggle = {purpleToggle}  greenToggle = {greenToggle} yellowToggle = {yellowToggle} blueToggle = {blueToggle} grayToggle = {grayToggle}/>
+    <TextForm title="About Us" mode = {mode}  toggleMode = {toggleMode} redToggle = {redToggle} greenToggle = {greenToggle} purpleToggle = {purpleToggle} yellowToggle = {yellowToggle} grayToggle = {grayToggle}/>
     </>
   );
 }
-
-
-
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,53 @@ import React from 'react'
 
 export default function Navbar(props) {
     return (
-        <div>
-            <nav class={`navbar navbar-expand-lg  bg-${props.mode}  navbar-${props.mode}`} id='Surajyy'>
-                <div class={"container-fluid"}>
-                    <a class="navbar-brand" href="#">{props.Mytitle}</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
+        <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">{props.heading}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    
+                    <ul className={`navbar-nav me-auto mb-2 mb-lg-0 text-${props.mode === 'black' ? 'yellow' : 'red'}`}>
+
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-disabled="true">{props.AboutText}</a>
+                        </li>
+
+                        <div className="dropdown">
+                        <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" >
+                         Theme
+                        </button>
+                        <ul className="dropdown-menu" id="cir">
+                           <div className="circle-1" onClick={props.redToggle}><p className="font-1">Red</p></div>
+                           <div className="circle-2" onClick={props.greenToggle}><p className="font-2">Green</p></div>
+                           <div className="circle-3" onClick={props.purpleToggle}><p className="font-3">Purple</p></div>
+                           <div className="circle-4" onClick={props.yellowToggle}><p className="font-4">O & Y</p></div>
+                           <div className="circle-5" onClick={props.blueToggle}><p className="font-5">Blue</p></div>
+                           <div className="circle-6" onClick={props.grayToggle}><p className="font-6">Gray</p></div>
+
+
                            
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
                         </ul>
-                        <form class="d-flex" role="search">
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch" onClick = {props.ToggleTheam}  id="flexSwitchCheckDefault" />{props.thisBtn}
-                            </div>
-                        </form>
                     </div>
+                    </ul>
+                   
+                    <form className="d-flex" role="search">
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />{props.btnText}
+                        </div>
+                    </form>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
+
     )
 }
